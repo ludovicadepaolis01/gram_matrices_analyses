394722c2263539e19ac91e5c631c82a89e671eb6
ludovicadepaolis01 <ldepaoli@sissa.it>
2026-02-16 18:39:48 +0100

    fixed var

--- jobs/par_optim_test.sh
+++ jobs/par_optim_test.sh
@@ -11,7 +11,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=/leonardo/home/userexternal/ldepaoli/lab/gram_matrices_analyses/out/par_optim_test_%x.%A.%a.out
 #SBATCH --error=/leonardo/home/userexternal/ldepaoli/lab/gram_matrices_analyses/out/par_optim_test_%x.%A.%a.err
-#SBATCH --array=0-13 #total number of models
+#SBATCH --array=0-64 #total number of models
 
 module purge #unload any previously loaded modules to use a clean venv
 module load profile/deeplrn
@@ -19,9 +19,13 @@ module load cineca-ai/4.3.0
 source $HOME/virtualenvs/dl/bin/activate
 
 MODELS=(vgg16 vgg19 alexnet resnet18 resnet34 resnet50 resnet101 resnet152 inceptionv3 mobilenet densenet121 densenet169 densenet201)
-MODEL_NAME="${MODELS[$SLURM_ARRAY_TASK_ID]}"
-echo "Running model: $MODEL_NAME"
+TEXTURES=(blotchy matted pebble scaly striped)
+
+N_TEXT=${#TEXTURES[@]}
+MODEL_NAME="${MODELS[$((SLURM_ARRAY_TASK_ID / N_TEXT))]}"
+TEXTURE_NAME="${TEXTURES[$((SLURM_ARRAY_TASK_ID % N_TEXT))]}"
 
 cd "$HOME/lab/gram_matrices_analyses/analyses"
+echo "Running model: $MODEL_NAME  texture: $TEXTURE_NAME"
+python -u optimization_test.py --model "$MODEL_NAME" --texture "$TEXTURE_NAME"
 
-python -u optimization_test.py --model "$MODEL_NAME"
