4a3b7025cd8605a53838a1b2d48b32653a518ccc
ludovicadepaolis01 <ldepaoli@sissa.it>
2026-02-20 18:38:09 +0100

    alexnet feat ext

--- models/alexnet_arch.py
+++ models/alexnet_arch.py
@@ -14,68 +14,59 @@ OMP_NUM_THREADS=1
 #alexnet_pretrained = models.alexnet()
 #print(alexnet_pretrained)
 
+layer_indices = [0, 3, 6, 8, 10]
+
 #alexnet
 class alexnet_representations(nn.Module):
      model_path = "/leonardo/home/userexternal/ldepaoli/models/alexnet-owt-7be5be79.pth"
-     def __init__(self): #images
+     def __init__(self, selected_idx: int): #images
           super().__init__() #refers to the class that this class inherits from (nn.Modules)
+          self.selected_idx = int(selected_idx)
           self.alexnet_pretrained = models.alexnet()
+          self.alexnet_pretrained.eval()
           state_dict = torch.load(alexnet_representations.model_path)
           self.alexnet_pretrained.load_state_dict(state_dict)
           self.alexnet_pretrained.to("cuda")
           self.alexnet_pretrained.requires_grad_(False) #to not compute gradients with respect to model parameters
-          self.hooks = []
+          self.feature_maps = {}     
+          self._hooks = []
+
+          for idx, layer in enumerate(self.alexnet_pretrained.features):
+               if isinstance(layer, nn.Conv2d) and (idx in layer_indices):
+                    layer._hook_idx = idx
+                    h = layer.register_forward_hook(self._hook_func)
+                    self._hooks.append(h)
+
+     def _hook_func(self, module, inp, out):
+          idx = module._hook_idx
+          self.feature_maps[idx] = out.detach()
+
+     def gram_matrix(self, feature_map: torch.Tensor) -> torch.Tensor:
+          return torch.einsum("bihw,bjhw->bij", feature_map, feature_map)
+
+     def forward(self, images: torch.Tensor):
+          # reset per forward
           self.feature_maps = {}
 
-          count = 0
-          #for (name, layer) in alexnet.features.named_modules(): #this is useful to extract only the ReLU of the of the conv not of the classifier
-          for (idx, layer) in enumerate(self.alexnet_pretrained.features):
-               #print(idx)
-               #print(layer)
-               if isinstance(layer, torch.nn.Conv2d):
-                    if idx in [0, 3, 6, 8, 10]:
-                    #if prev_layer is None or isinstance(prev_layer, nn.MaxPool2d):
-                         hook = layer.register_forward_hook(self.hook_func)
-                         layer.name = f'layer_{count}'
-                         #print(layer.name)
-                         self.hooks.append(hook)
-               #prev_layer = layer
-               count += 1
+          _ = self.alexnet_pretrained(images)
 
-     def hook_func(self, module, input, output): #all of the three arguments are necessary
-          name = module.name
-          #print(f"Hook fired for: {name}")
-          #feature_maps = self.feature_maps
-          self.feature_maps[name] = output#.detach()
-          
-     def gram_matrix(self, feature_map):
-          #for tensor in feature_map:
-          gram_matrix = torch.einsum("bihw,bjhw->bij", feature_map, feature_map)
-          
-          return gram_matrix
-     
-     def forward(self, images):
-          gram_matrix_list = []
-          feature_map_m_list = []
-          feature_map_n_list = []
-          feature_map_list = []
-          model = self.alexnet_pretrained(images)
+          grams_list = []
+          m_list = []
+          feature_maps_list = []
 
-          #check if gram matrices (list) are not transferred from gpu to cpu from time to time
-          #this could slow down the whole learning process
-          #
-          for key in self.feature_maps:
-               feature_map = self.feature_maps[key]
-               feature_map_height = feature_map.size(2)
-               feature_map_width = feature_map.size(3)
-               feature_map_m = feature_map_height*feature_map_width
-               feature_map_m_list.append(feature_map_m)
-               gram_matrices = self.gram_matrix(feature_map)
-               gram_matrix_list.append(gram_matrices)
-               feature_map_list.append(feature_map)
-               
-          return gram_matrix_list, feature_map_m_list, feature_map_list
-     
+          # deterministic order
+          for idx in layer_indices:
+               fmap = self.feature_maps[idx]  # (B,C,H,W)
+               feature_maps_list.append(fmap)
+               m_list.append(int(fmap.size(2) * fmap.size(3)))
+               grams_list.append(self.gram_matrix(fmap))
+
+          # selected outputs
+          sel_fmap = self.feature_maps[self.selected_idx]
+          sel_gram = self.gram_matrix(sel_fmap)
+
+          return grams_list, m_list, feature_maps_list, sel_gram, sel_fmap
+
 def gaussian_image_tensor(size=400, mean=0.5, std=0.2):
     gaussian_image = torch.randn(3, size, size) * std + mean
     gaussian_image.clamp_(0.0, 1.0)
